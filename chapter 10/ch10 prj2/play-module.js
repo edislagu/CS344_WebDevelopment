4658ddd14d032a827ce2300d5ce79726b768b813
edislagu <edwinfc826@gmail.com>
2026-04-21 19:41:32 -0500

    changes to project 2 of chapter 10

---
+++ chapter 10/ch10 prj2/play-module.js
@@ -0,0 +1,95 @@
+class Play {
+    constructor(playData) {
+        this.acts = [];
+        this.players = [];
+
+        this.title = playData.title;
+        this.short = playData.short;
+
+        playData.persona.forEach( p => { this.players.push(p)});
+
+        playData.acts.forEach(a => {
+            this.acts.push( new Act(a) );
+
+        });
+    }
+
+    output(playContainer, actContainer, sceneContainer) {
+        playContainer.textContent = this.title;
+        this.acts[0].output(actContainer, sceneContainer);
+    }
+}
+
+class Act {
+    constructor(actData) {
+        this.name = actData.name;
+        this.scenes = [];
+        actData.scenes.forEach( s => {
+            this.scenes.push( new Scenes(s.name, s.title, s.stageDirection, s.speeches));
+        })       
+    }
+
+    output(actContainer, sceneContainer) {
+        actContainer.textContent = this.name;
+        this.scenes[0].output(sceneContainer);
+    }
+}
+
+class Scene {
+    
+    constructor(name,title,direction,speeches) {
+        this.name = name;
+        this.title = title;
+        this.direction = direction;
+        this.speeches = speeches;
+    }
+
+    output(containers, search, players) {
+        containers.innerHTML = "";
+        const h4 = document.createElement('h4');
+        containers.appendChild(h4);
+        const pTitle = document.createElement('p');
+        pTitle.classList.add('title');
+        pTitle.textContent = this.title;
+        containers.appendChild(pTitle);
+        const pDirection = document.createElement('p');
+        pDirection.classList.add('direction');
+        pDirection.textContext = this.direction;
+        containers.appendChild(pDirection);
+
+        this.outputSpeeches(container, search, player);
+    }
+
+     outputSpeeches(container, search, player) {
+      const divs = this.speeches.map( sp => {
+
+
+         const div = document.createElement('div');
+         div.classList.add('speech');
+         if (player && sp.speaker != player && player != 0) return div;
+         const span = document.createElement('span');
+         span.textContent = sp.speaker;
+         div.appendChild(span);
+         const lines = sp.lines.map( line => {
+            const p = document.createElement('p');
+            if (search) {
+               const newLine = line.replace(new RegExp(search, "i"),`<b>${search}</b>`);
+               p.innerHTML = newLine;
+            } else {
+               p.textContent = line;
+            }            
+            return p;
+          } );
+         lines.forEach( line => { div.appendChild(line) } );
+         if (sp.stagedir) {
+            const em = document.createElement('em');
+            em.textContent = sp.stagedir;
+            div.appendChild(em);
+         }
+         return div;
+      });
+
+      divs.forEach( div => { container.appendChild(div) } );
+   }
+}
+
